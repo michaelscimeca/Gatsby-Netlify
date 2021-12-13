e05057fe2f0c6c9184201b7c3a37f1fa44b16d3a
michaelscimeca <mikey@rsq.com>
2021-12-13 09:40:44 -0600

    update evn

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,8 +46,8 @@ module.exports = {
     {
       resolve: "gatsby-source-sanity",
       options: {
-        projectId: process.env.SANITY_PROJECT_ID,
-        dataset: process.env.SANITY_DATASET,
+        projectId: process.env.GATSBY_SANITY_PROJECT_ID,
+        dataset: process.env.GATSBY_SANITY_DATASET ,
         watchMode: true,
         graphqlTag: 'default',
         // token: process.env.SANITY_TOKEN,
